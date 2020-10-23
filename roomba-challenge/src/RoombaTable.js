8f2682b9aef6ed703bacb4d2eafdf96470072ffb
sinanbayoud <sinanbayoud@gmail.com>
2020-10-22 22:13:58 -0700

    Movement, walls hit, distance travelled finished

--- roomba-challenge/src/RoombaTable.js
+++ roomba-challenge/src/RoombaTable.js
@@ -15,22 +15,80 @@ const useStyles = makeStyles({
   },
 });
 
+var totalDistance = 0;
+var wallsHit = 0;
+
+//TODO
+function updateLocation(currentLocation, currentAction, roomDimensions) {
+    var xLimit = roomDimensions[0];
+    var yLimit = roomDimensions[1];
+
+    switch(currentAction) {
+        case "N":
+            if (currentLocation[1]+1 > yLimit) {
+                wallsHit++;
+                return [currentLocation[0],currentLocation[1]];
+            }
+            totalDistance++;
+            return [currentLocation[0],currentLocation[1]+1];
+        case "S":
+            if (currentLocation[1]-1 < 0) {
+                wallsHit++;
+                return [currentLocation[0],currentLocation[1]];
+            }
+            totalDistance++;
+            return [currentLocation[0],currentLocation[1]-1];
+        case "E":
+            if (currentLocation[0]+1 > xLimit) {
+                wallsHit++;
+                return [currentLocation[0],currentLocation[1]];
+            }
+            totalDistance++;
+            return [currentLocation[0]+1,currentLocation[1]];
+        case "W":
+            if (currentLocation[0]-1 < 0) {
+                wallsHit++;
+                return [currentLocation[0],currentLocation[1]];
+            }
+            totalDistance++;
+            return [currentLocation[0]-1,currentLocation[1]];
+        default:
+            console.error("invalid step detected, ignoring");
+            return [currentLocation[0],currentLocation[1]];
+    }
+}
+
+//TODO
+// function checkDirt(location) {
+//     return true;
+// }
+
 function createData(step, location, action, dirtCollected, wallsHit) {
     return { step, location, action, dirtCollected, wallsHit };
 }
 
-
 const RoombaTable = ({ data }) => {
     var rows = [];
+    totalDistance = 0;
+    wallsHit = 0;
     
     const classes = useStyles();
     var actionList = data.drivingInstructions;
+    var currentLocation = data.initialRoombaLocation;
+    var roomDimensions = data.roomDimensions;
+
     if (data.drivingInstructions !== undefined) {
-        for (var index = 0; index < actionList.length; index++) {
-            rows.push(createData(index, 0, actionList[index], 0, 0));
+        rows.push(createData(1, currentLocation[0] + ", " + currentLocation[1], "", 0, wallsHit));
+        
+        for (var index = 1; index < actionList.length+1; index++) {
+            var currentAction = actionList[index-1];
+            currentLocation = updateLocation(currentLocation, currentAction, roomDimensions);
+            rows.push(createData(index+1, currentLocation[0] + ", " + currentLocation[1], currentAction, 0, wallsHit));
         }
-        console.log(rows);
+
+        var finalPosition = currentLocation[0] + ", " + currentLocation[1];
     }
+
 
     return (
     <TableContainer component={Paper}>
@@ -57,6 +115,10 @@ const RoombaTable = ({ data }) => {
             ))}
             </TableBody>
         </Table>
+        <h1>Final Position: {finalPosition}</h1>
+        <h1>Total Distance Travelled: {totalDistance}</h1>
+        <h1>Total Dirt Collected: TODO</h1>
+        <h1>Total Walls Hit: {wallsHit}</h1>
     </TableContainer>
     );
 }
